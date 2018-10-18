f59326d466f092815285a2f8d0ae502a2c7169c8
Nick Bennett <nick@nickbenn.com>
2018-10-18 12:01:14 -0600

    RollCell for ListView<Roll> population, dice images, icon, accelerators.

--- src/edu/cnm/deepdive/craps/controller/Controller.java
+++ src/edu/cnm/deepdive/craps/controller/Controller.java
@@ -2,8 +2,12 @@ package edu.cnm.deepdive.craps.controller;
 
 import edu.cnm.deepdive.craps.model.Game;
 import edu.cnm.deepdive.craps.model.Game.Roll;
+import edu.cnm.deepdive.craps.model.Game.State;
+import edu.cnm.deepdive.craps.view.RollCell;
 import java.security.SecureRandom;
 import java.util.List;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
@@ -12,10 +16,16 @@ import javafx.scene.text.Text;
 
 public class Controller {
 
+  private static final String PLACEHOLDER_STYLE_CLASS = "";
+  private static final String WIN_STYLE_CLASS = "win";
+  private static final String LOSS_STYLE_CLASS = "loss";
+
   private Game game;
   private boolean running;
   private String tallyFormat;
 
+  @FXML
+  private ResourceBundle resources;
   @FXML
   private MenuItem next;
   @FXML
@@ -25,14 +35,17 @@ public class Controller {
   @FXML
   private MenuItem reset;
   @FXML
-  private ListView rolls;
+  private ListView<Roll> rolls;
   @FXML
   private Text tally;
 
   @FXML
   private void initialize() {
     tallyFormat = tally.getText();
+    rolls.setCellFactory((lv) -> new RollCell(resources));
+    rolls.getStyleClass().add(PLACEHOLDER_STYLE_CLASS);
     reset(null);
+    updateMenu();
   }
 
   @FXML
@@ -45,14 +58,13 @@ public class Controller {
   @FXML
   private void fast(ActionEvent actionEvent) {
     running = true;
-    // TODO Update menu items enabled/disabled
-    // TODO Instantiate and start Runner
+    updateMenu();
+    new Runner().start();
   }
 
   @FXML
   private void pause(ActionEvent actionEvent) {
     running = false;
-    // TODO Release Runner instance
   }
 
   @FXML
@@ -62,14 +74,77 @@ public class Controller {
     updateRolls(game.getRolls());
   }
 
+  public void stop() {
+    pause(null);
+  }
+
   private void updateTally(int wins, int losses) {
     int total = wins + losses;
     double percentage = (total != 0) ? (100.0 * wins / total) : 0;
     tally.setText(String.format(tallyFormat, wins, total, percentage));
   }
 
-  private void updateRolls(List<Roll> rolls) {
-    // TODO Update Rolls ListView.
+  private void updateRolls(List<Roll> diceRolls) {
+    List<String> styleClasses = rolls.getStyleClass();
+    styleClasses.remove(styleClasses.size() - 1);
+    rolls.getItems().clear();
+    if (!diceRolls.isEmpty()) {
+      State state = diceRolls.get(diceRolls.size() - 1).getState();
+      if (state == State.WIN) {
+        styleClasses.add(WIN_STYLE_CLASS);
+      } else {
+        styleClasses.add(LOSS_STYLE_CLASS);
+      }
+      rolls.getItems().addAll(diceRolls);
+    } else {
+      styleClasses.add(PLACEHOLDER_STYLE_CLASS);
+    }
+  }
+
+  private void updateMenu() {
+    next.setDisable(running);
+    fast.setDisable(running);
+    pause.setDisable(!running);
+    reset.setDisable(running);
+  }
+
+  private class Runner extends Thread {
+
+    private static final int TALLY_UPDATE_INTERVAL = 5_000;
+    private static final int ROLLS_UPDATE_INTERVAL = 20_000;
+
+    @Override
+    public void run() {
+      int count = 0;
+      while (running) {
+        game.play();
+        count++;
+        if (count % TALLY_UPDATE_INTERVAL == 0) {
+          int wins = game.getWins();
+          int losses = game.getLosses();
+          Platform.runLater(() -> updateTally(wins, losses));
+        }
+        if (count % ROLLS_UPDATE_INTERVAL == 0) {
+          List<Roll> rolls = game.getRolls();
+          Platform.runLater(() -> updateRolls(rolls));
+        }
+      }
+      int wins = game.getWins();
+      int losses = game.getLosses();
+      List<Roll> rolls = game.getRolls();
+      Platform.runLater(() -> {
+        updateTally(wins, losses);
+        updateRolls(rolls);
+        updateMenu();
+      });
+    }
+
   }
 
 }
+
+
+
+
+
+
---
+++ test/edu/cnm/deepdive/craps/model/GameTest.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.craps.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+  @Test
+  void stateChanges() {
+
+  }
+}
