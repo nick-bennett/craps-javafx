a6e36006b276dc8029801031f55d7f3f10655dc0
Nick Bennett <nick@nickbenn.com>
2018-10-22 12:33:35 -0600

    More unit testing.

--- test/edu/cnm/deepdive/craps/model/GameTest.java
+++ test/edu/cnm/deepdive/craps/model/GameTest.java
@@ -6,7 +6,6 @@ import edu.cnm.deepdive.craps.model.Game.State;
 import java.util.stream.Stream;
 import java.util.stream.Stream.Builder;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
